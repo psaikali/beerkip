41e36d3476f2684b9bcd5138aec1fdb50abc41c0
Pierre Saïkali <dev@mosaika.fr>
2019-08-22 14:44:28 +0200

    🆕 ADD: basic styling and logo on the Login screen.

--- src/screens/Login/Login.js
+++ src/screens/Login/Login.js
@@ -1,22 +1,47 @@
 import React, { Component } from "react";
-import { View } from "react-native";
+import { View, Image, StyleSheet } from "react-native";
 import { Button, Text } from "native-base";
+
+import logo from "../../assets/images/beerkip-logo-white.png";
+import COLORS from "../../utils/colors";
 
 class Login extends Component {
 	render() {
 		return (
-			<View>
-				<Text>Login screen</Text>
-				<Button
-					onPress={() => {
-						this.props.navigation.navigate("BeersList");
-					}}
-				>
-					<Text>Go to app</Text>
-				</Button>
+			<View style={styles.screenContentWrapper}>
+				<View style={styles.contentBox}>
+					<Image source={logo} style={styles.logo} />
+
+					<Button
+						bordered
+						light
+						onPress={() => {
+							this.props.navigation.navigate("BeersList");
+						}}
+					>
+						<Text>Go to app</Text>
+					</Button>
+				</View>
 			</View>
 		);
 	}
 }
+
+const styles = StyleSheet.create({
+	screenContentWrapper: {
+		flex: 1,
+		backgroundColor: COLORS.purple,
+		justifyContent: "center",
+	},
+	contentBox: {
+		marginHorizontal: 50,
+	},
+	logo: {
+		height: undefined,
+		width: "100%",
+		aspectRatio: 507 / 102,
+		marginBottom: 20,
+	},
+});
 
 export default Login;
---
+++ src/utils/colors.js
@@ -0,0 +1,4 @@
+export default (COLORS = {
+	purple: "#5523A5",
+	yellow: "#F4CE00",
+});
