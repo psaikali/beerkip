2717dd4887c617e513e9eee1dcb5ec63912faad5
Pierre Saïkali <dev@mosaika.fr>
2019-08-22 14:44:51 +0200

    👌 OPTIM: adding a prop to ScreenContent to pass extra styles

--- src/components/ScreenContent/ScreenContent.js
+++ src/components/ScreenContent/ScreenContent.js
@@ -4,7 +4,16 @@ import { Container, Content } from "native-base";
 import STYLES from "../../utils/styles.js";
 
 export default (ScreenContent = props => (
-	<Container style={STYLES.container}>
-		<Content>{props.children}</Content>
+	<Container
+		style={[
+			STYLES.container,
+			props.containerStyle ? props.containerStyle : {},
+		]}
+	>
+		<Content
+			contentContainerStyle={props.contentStyle ? props.contentStyle : {}}
+		>
+			{props.children}
+		</Content>
 	</Container>
 ));
