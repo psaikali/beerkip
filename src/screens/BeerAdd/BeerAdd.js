a81cdf8a46852f1f2bc8b3efb8760ea2a34466f2
Pierre Saïkali <dev@mosaika.fr>
2019-08-23 17:46:53 +0200

    🆕 ADD: submitting our BeerAddForm now creates a new beer with the Redux addBeer() action

--- src/screens/BeerAdd/BeerAdd.js
+++ src/screens/BeerAdd/BeerAdd.js
@@ -1,17 +1,47 @@
 import React, { Component } from "react";
 import { View } from "react-native";
-import { Text } from "native-base";
+import { Text, H3 } from "native-base";
+import { connect } from "react-redux";
+import UUIDGenerator from "react-native-uuid-generator";
 
 import ScreenContent from "../../components/ScreenContent/ScreenContent";
+import BeerAddForm from "../../components/BeerAddForm/BeerAddForm";
+
+import { addBeer } from "../../store/actions/beers";
+
+import dummyBeerImage from "../../assets/images/dummy/beer3.png";
 
 class BeerAdd extends Component {
+	handleBeerAddFormSubmit = values => {
+		UUIDGenerator.getRandomUUID().then(uid => {
+			this.props.addBeer({
+				uid: uid,
+				createdAt: Date.now(),
+				editedAt: null,
+				deletedAt: null,
+				photo: dummyBeerImage,
+				...values,
+			});
+		});
+	};
+
 	render() {
 		return (
 			<ScreenContent>
-				<Text>Beer add</Text>
+				<H3>New beer</H3>
+				<BeerAddForm onSubmit={this.handleBeerAddFormSubmit} />
 			</ScreenContent>
 		);
 	}
 }
 
-export default BeerAdd;
+const mapDispatchToProps = dispatch => {
+	return {
+		addBeer: data => dispatch(addBeer(data)),
+	};
+};
+
+export default connect(
+	null,
+	mapDispatchToProps
+)(BeerAdd);
