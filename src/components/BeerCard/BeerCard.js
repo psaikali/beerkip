2695af62be35c232a0e0c171a6cb35481c6101e5
Pierre Saïkali <dev@mosaika.fr>
2019-09-03 15:19:00 +0200

    👌 OPTIM: displaying a small Refresh icon on top of beer thumbnail, if it's not synced yet.

--- src/components/BeerCard/BeerCard.js
+++ src/components/BeerCard/BeerCard.js
@@ -1,14 +1,19 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
-import { ListItem, Body, Left, Right, Thumbnail } from "native-base";
+import { ListItem, Icon, Body, Left, Right, Thumbnail } from "native-base";
 
 export default BeerCard = ({ item, onPress }) => (
 	<ListItem thumbnail key={item.uid} button onPress={onPress}>
 		<Left>
 			<Thumbnail square source={item.photo} />
+			{item.edited && (
+				<View style={styles.beerNeedsSync}>
+					<Icon name="refresh" style={styles.beerNeedsSyncIcon} />
+				</View>
+			)}
 		</Left>
 		<Body>
-			<Text>{item.name}</Text>
+			<Text style={styles.beerName}>{item.name}</Text>
 			<Text note numberOfLines={1}>
 				{item.comment}
 			</Text>
@@ -22,6 +27,27 @@ export default BeerCard = ({ item, onPress }) => (
 );
 
 const styles = StyleSheet.create({
+	beerNeedsSync: {
+		position: "absolute",
+		backgroundColor: COLORS.orange,
+		top: 3,
+		right: 3,
+		borderRadius: 15,
+		width: 15,
+		height: 15,
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	beerNeedsSyncIcon: {
+		width: 10,
+		height: 10,
+		fontSize: 10,
+		color: "#ffffff",
+		marginLeft: 2,
+	},
+	beerName: {
+		fontWeight: "bold",
+	},
 	beerRating: {
 		backgroundColor: COLORS.gray,
 		borderRadius: 50,
