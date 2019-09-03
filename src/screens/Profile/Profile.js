d92f330ec912c5cce5f30fbe70424c15d0e3e043
Pierre Saïkali <dev@mosaika.fr>
2019-09-03 15:33:42 +0200

    🆕 ADD: basic display of WP user data in the Profile screen

--- src/screens/Profile/Profile.js
+++ src/screens/Profile/Profile.js
@@ -1,17 +1,44 @@
 import React, { Component } from "react";
-import { View } from "react-native";
-import { Text } from "native-base";
+import { StyleSheet } from "react-native";
+import { H3, Text, Left, Right, List, ListItem } from "native-base";
+import { connect } from "react-redux";
 
 import ScreenContent from "../../components/ScreenContent/ScreenContent";
 
-class Login extends Component {
+class Profile extends Component {
 	render() {
+		const { login, email, name } = this.props.user;
+
 		return (
 			<ScreenContent>
-				<Text>User profile</Text>
+				<H3>User profile</H3>
+				<List>
+					<ListItem>
+						<Text style={styles.listLabel}>Login: </Text>
+						<Text>{login}</Text>
+					</ListItem>
+					<ListItem>
+						<Text style={styles.listLabel}>E-mail address: </Text>
+						<Text>{email}</Text>
+					</ListItem>
+					<ListItem>
+						<Text style={styles.listLabel}>Name: </Text>
+						<Text>{name}</Text>
+					</ListItem>
+				</List>
 			</ScreenContent>
 		);
 	}
 }
 
-export default Login;
+const styles = StyleSheet.create({
+	listLabel: {
+		fontWeight: "bold",
+	},
+});
+
+const mapStateToProps = state => ({
+	user: state.app.user,
+});
+
+export default connect(mapStateToProps)(Profile);
