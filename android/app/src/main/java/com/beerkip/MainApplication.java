64fa4e5286714d5d8e0d4a67b206f1fde2491309
Pierre Saïkali <dev@mosaika.fr>
2019-09-04 18:03:42 +0200

    🆕 ADD: adding codePush library + config. 🐛 FIX: adding release signing so that app can be installed on a smartphone without warning

--- android/app/src/main/java/com/beerkip/MainApplication.java
+++ android/app/src/main/java/com/beerkip/MainApplication.java
@@ -7,6 +7,7 @@ import com.facebook.react.PackageList;
 import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
 import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.ReactApplication;
+import com.microsoft.codepush.react.CodePush;
 import com.reactnativecommunity.netinfo.NetInfoPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -17,6 +18,11 @@ import java.util.List;
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    protected String getJSBundleFile(){
+      return CodePush.getJSBundleFile();
+    }
+
     @Override
     public boolean getUseDeveloperSupport() {
       return BuildConfig.DEBUG;
