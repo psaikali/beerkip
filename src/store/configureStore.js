eda0237c2e5134077e0db2e6a4741451999c0f80
Pierre Saïkali <dev@mosaika.fr>
2019-11-12 18:34:40 +0100

    👌 OPTIM: simplification du store

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,4 +1,4 @@
-import { createStore, combineReducers, compose, applyMiddleware } from "redux";
+import { createStore, combineReducers } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
 import FilesystemStorage from "redux-persist-filesystem-storage";
 
@@ -14,12 +14,6 @@ const rootReducer = combineReducers({
 });
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-let composeEnhancers = compose;
-
-if (__DEV__) {
-	composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-}
 
 export const store = createStore(persistedReducer);
 
