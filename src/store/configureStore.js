d8b82556f0536618d5af897c6a25db6899b74c4c
Pierre Saïkali <dev@mosaika.fr>
2019-11-12 17:17:22 +0100

    👌 OPTIM: suppression code commenté dans configuration du store

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -3,19 +3,14 @@ import { persistStore, persistReducer } from "redux-persist";
 import FilesystemStorage from "redux-persist-filesystem-storage";
 
 import beersReducer from "./reducers/beers";
-// import appReducer from "./reducers/app";
-// import categoriesReducer from "./reducers/categories";
 
 const persistConfig = {
 	key: "brkp",
 	storage: FilesystemStorage,
-	//blacklist: ["form"]
 };
 
 const rootReducer = combineReducers({
 	beers: beersReducer,
-	// app: appReducer,
-	// categories: categoriesReducer,
 });
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
@@ -26,9 +21,6 @@ if (__DEV__) {
 	composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 }
 
-export const store = createStore(
-	persistedReducer
-	//composeEnhancers(applyMiddleware(thunk))
-);
+export const store = createStore(persistedReducer);
 
 export const persistor = persistStore(store);
